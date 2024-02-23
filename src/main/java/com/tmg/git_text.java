e1eef94df0d65015ae507fbbf148fce3d0afb8ad
Robin <8281700+rrrobin@user.noreply.gitee.com>
2024-02-23 20:43:13 +0800

    onebranch测试冲突添加一行

--- src/main/java/com/tmg/git_text.java
+++ src/main/java/com/tmg/git_text.java
@@ -5,5 +5,6 @@ public class git_text {
         System.out.println("hello git 1");
         System.out.println("hello git 2");
         System.out.println("hello git 3 onebranch第一次添加");
+        System.out.println("hello git 3 onebranch测试冲突添加这一行");
     }
 }

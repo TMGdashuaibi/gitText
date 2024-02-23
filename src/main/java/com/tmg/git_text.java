09f51125c2b70bb7dbf218742c8ae7b3ed1d8262
Robin <8281700+rrrobin@user.noreply.gitee.com>
2024-02-23 20:42:19 +0800

    onebranch第一次修改（添加了一行）

--- src/main/java/com/tmg/git_text.java
+++ src/main/java/com/tmg/git_text.java
@@ -5,5 +5,7 @@ public class git_text {
         System.out.println("hello git 1");
         System.out.println("hello git 2");
         System.out.println("hello git 3 onebranch第一次添加");
+        System.out.println("hello git 3 master测试冲突添加了这一行");
+
     }
 }
